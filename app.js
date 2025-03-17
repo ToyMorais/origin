5f8fc806d9822a854c9fca424c0c0abf2330311a
ToyMorais <ajcmoraisfilho@gmail.com>
2025-03-16 21:12:39 -0300

    Diminuir número de tentativas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;  // Número máximo que o número secreto pode ser
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
